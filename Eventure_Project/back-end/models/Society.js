9e909ebb78ec86b0a90f78b76fbf200cd8c5d010
hayatbounite72@gmail.com <hayatbounite72@gmail.com>
2025-03-14 16:43:28 +0200

    updated society model to link event

--- Eventure_Project/back-end/models/Society.js
+++ Eventure_Project/back-end/models/Society.js
@@ -1,9 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const societySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: String,
-  president: { type: mongoose.Schema.Types.ObjectId, ref: "Organizer" },
+  logo: { type: String, required: true },
+  description: { type: String, required: true },
+  location: { type: String },
+  contact: { type: String },
+  // This will store the list of events related to the society
+  events: [
+    {
+      event_name: { type: String, required: true },
+      event_date: { type: Date, required: true },
+      event_description: { type: String, required: true },
+    },
+  ],
+  // This will store organizers of the society (optional)
+  organizers: [
+    {
+      name: { type: String, required: true },
+      role: { type: String, required: true },
+    },
+  ],
 });
 
-module.exports = mongoose.model("Society", societySchema);
+// Create the Society model
+const Society = mongoose.model('Society', societySchema);
+
+module.exports = Society;
