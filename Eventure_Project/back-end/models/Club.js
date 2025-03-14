4b2aa38a2f120ab10d6fac3422c9ed3395fb5cfa
hayatbounite72@gmail.com <hayatbounite72@gmail.com>
2025-03-14 16:26:16 +0200

    changed club model to link to event

--- Eventure_Project/back-end/models/Club.js
+++ Eventure_Project/back-end/models/Club.js
@@ -1,11 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const clubSchema = new mongoose.Schema({
   name: { type: String, required: true },
   logo: { type: String, required: true },
   description: { type: String, required: true },
-  events: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }],
-  organizer: { type: mongoose.Schema.Types.ObjectId, ref: "Organizer" },
+  location: { type: String },
+  contact: { type: String },
+  // This will store the list of events related to the club
+  events: [
+    {
+      event_name: { type: String, required: true },
+      event_date: { type: Date, required: true },
+      event_description: { type: String, required: true },
+    },
+  ],
+  // This will store organizers of the club (optional)
+  organizers: [
+    {
+      name: { type: String, required: true },
+      role: { type: String, required: true },
+    },
+  ],
 });
 
-module.exports = mongoose.model("Club", clubSchema);
+// Create the Club model
+const Club = mongoose.model('Club', clubSchema);
+
+module.exports = Club;
