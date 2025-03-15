2aabe42156d03e828c6fc9b376d2f247d2779a27
hayatbounite72@gmail.com <hayatbounite72@gmail.com>
2025-03-15 12:23:35 +0200

    added events to faculty

--- Eventure_Project/back-end/models/Faculty.js
+++ Eventure_Project/back-end/models/Faculty.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 const facultySchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
+  events: [
+    {
+      eventName: { type: String, required: true },
+      date: { type: Date, required: true },
+      location: { type: String, required: true }
+    }
+  ]
 });
 
 module.exports = mongoose.model("Faculty", facultySchema);
